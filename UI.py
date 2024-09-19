cdb421662c86886640ac98b660cbc2c291fc5f16
yobysheva <bobysheva.yv@students.dvfu.ru>
2024-09-19 18:07:31 +1000

    потоки главного окна и сервера

--- UI.py
+++ UI.py
@@ -1,8 +1,63 @@
-from PyQt6.QtWidgets import QWidget, QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QSize
 from PyQt6 import uic
 import random
+import socket
+import threading
+
+def server():
+    # Создаем сокет
+    serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+    serv_sock.bind(('', 53210))
+    serv_sock.listen(10)
+    current_user = 0
+
+    try:
+        while True:
+            # Бесконечно обрабатываем входящие подключения
+            client_sock, client_addr = serv_sock.accept()
+
+            print('client connected', client_addr)
+            # карточки ненадолго переворачиваются
+            while True:
+                match current_user:
+                    case 0:
+                        # Получаем сообщение от клиента
+                        data = client_sock.recv(1024)
+                        if not data:  # Клиент отключился
+                            break
+                        message1 = data.decode('utf-8')
+                        print(message1)
+
+                        data = client_sock.recv(1024)
+                        if not data:  # Клиент отключился
+                            break
+                        message2 = data.decode('utf-8')
+                        print(message2)
+                        # if message1.image != message2.image:
+                        #   current_user = 1
+
+                    case 1:
+                        # Отправляем сообщение клиенту
+                        response1 = input()
+                        client_sock.sendall(response1.encode('utf-8'))
+                        response2 = input()
+                        client_sock.sendall(response2.encode('utf-8'))
+                        # заблокировать все кнопки для нажатия, если неправильно
+                        # картинки открыть если правильно и продолжать отсылать
+                        # if response1.image != response2.image:
+                        #   current_user = 0
+
+            client_sock.close()
+
+    except (KeyboardInterrupt, SystemExit):
+        print("Прерывание программы.")
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -37,6 +92,11 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     import sys
+    # Запуск сервера в отдельном потоке
+    server_thread = threading.Thread(target=server, daemon=True)
+    server_thread.start()
+
+    # Запуск приложения PyQt
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
