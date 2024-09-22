b0e2feb8c78fde803dd35bb171df61216a596bdc
yobysheva <bobysheva.yv@students.dvfu.ru>
2024-09-23 00:29:31 +1000

    передаются данные от клиента к серверу, подрезаны картинки, все перекроено :)

--- client.py
+++ client.py
@@ -1,136 +1,60 @@
 import socket
-import sys
-
-from PyQt6.QtWidgets import QApplication
-from UI import MainWindow
-def client():
-    # Создаем сокет
-    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_sock.connect(('10.193.146.41', 53210))
-    # ненадолго перевернуть картинки
-    current_user = 0
-
-    try:
-        while True:
-            match current_user:
-                case 0:
-                    # Запрашиваем ввод сообщения от пользователя
-                    message1 = input("1 фото: ")
-                    # Отправляем сообщение серверу
-                    client_sock.sendall(message1.encode('utf-8'))
-                    message2 = input("2 фото: ")
-                    client_sock.sendall(message2.encode('utf-8'))
-                    if message1.image != message2.image:
-                       current_user = 1
-
-                case 1:
-                    # Получаем ответ от сервера
-                    data = client_sock.recv(1024)
-                    response1 = data.decode('utf-8')
-                    print(response1)
-                    data = client_sock.recv(1024)
-                    response2 = data.decode('utf-8')
-                    print(response2)
-                    # if response1.image != response2.image:
-                    #   current_user = 0
-
-
-    except (KeyboardInterrupt, SystemExit):
-        print("Прерывание программы.")
-
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
-
-    finally:
-        client_sock.close()
-
-
-if __name__ == "__main__":
-    client()
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
-
-from PyQt6.QtWidgets import QMainWindow, QApplication
+import threading
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QSize
 from PyQt6 import uic
 import random
-import socket
-import threading
 
 class MainWindow(QMainWindow):
     def __init__(self, current_user):
         super().__init__()
-        # Загрузить пользовательский интерфейс главного окна из файла .ui
         uic.loadUi('MainWindow.ui', self)
         self.setWindowTitle("Мемо - игра для вас и ваших друзей")
         self.setWindowIcon(QIcon('pictures/icon.png'))
 
+        # Подключение к серверу
         self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_sock.connect(('10.193.146.41', 53210))
+        self.client_sock.connect(('127.0.0.1', 53210))  # Укажите IP сервера
 
-        for button in self.buttons.buttons():
+        self.buttons_list = self.buttons.buttons()
+        self.current_user = current_user  # 0 - клиент, 1 - сервер
+        self.press_count = 0  # Количество нажатий
+
+        for button in self.buttons_list:
             button.setIcon(QIcon('pictures/back.jpg'))
             button.setText('')
-            button.setIconSize(QSize(button.width() - 6, button.height() - 6))
+            button.setIconSize(QSize(button.width() - 8, button.height() - 8))
             button.clicked.connect(self.button_client_clicked)
 
-        self.current_user = current_user
-        self.press_count = 0
+        self.your_turn.setStyleSheet("""QPushButton{
+                                        border: 5px solid #375;
+                                        border-style: outset;
+                                        background: rgb(60,150,90);
+                                        color: rgb(250,250,255);
+                                        }""")
         self.game_active = True  # Игра активна для клиента
-        threading.Thread(target=self.listen_to_server, daemon=True).start()
+        #threading.Thread(target=self.listen_to_server, daemon=True).start()
 
-
-    def listen_to_server(self):
-        while True:
-            data = self.client_sock.recv(1024)
-            pic = data.decode('utf-8')
-            button, pic1 = pic.split()
-            print(pic1)
-            button.setIcon(f'{pic1}')
-
-            data = self.client_sock.recv(1024)
-            pic = data.decode('utf-8')
-            button, pic2 = pic.split()
-            print(pic)
-            button.setIcon(f'{pic2}')
-
-            if pic1 != pic2:
-                break
-            else:
-                continue
-                # прописать про смену цветов рамки и блокировку выигранных карт
-
-
-    def button_client_clicked(self, current_user):
-        if self.current_user == 0 and self.press_count < 2 and self.game_active:
-            # сюда тоже можно вставить бесконечный цикл, и вместо запуска от кнопки, просто запускать его с самого начала
+    def button_client_clicked(self):
+        if self.game_active:  # Если ход клиента и игра активна
             button = self.sender()
-            self.press_count += 1
 
-            button_name = button.objectName()
-            self.client_sock.sendall(button_name.encode('utf-8'))
+            # Отправляем информацию о кнопке на сервер
+            button_info = f"{button.objectName()}"
+            self.client_sock.sendall(button_info.encode('utf-8'))
+
+            # Получаем путь к изображению от сервера и отображаем его
             data = self.client_sock.recv(1024)
-            pic = data.decode('utf-8')
-            button.setIcon(QIcon(f'{pic}'))
-
-            if self.press_count == 2:
-                self.current_user = 0
-                self.game_active = False
-                self.press_count = 0
-
-                self.listen_to_server()
-
-
-
-
+            picture = data.decode('utf-8')
+            print(picture)
+            button.setIcon(QIcon(picture))
+            button.setIconSize(QSize(button.width() - 8, button.height() - 8))
 
 
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
-    window = MainWindow(0)
+    window = MainWindow(0)  # 0 означает, что клиент ходит первым
     window.show()
     sys.exit(app.exec())
--- server.py
+++ server.py
@@ -1,103 +1,88 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication
+import socket
+import threading
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QSize
 from PyQt6 import uic
 import random
-import socket
-import threading
-
-def server():
-    # Создаем сокет
-    serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-    serv_sock.bind(('', 53210))
-    serv_sock.listen(1)
-    current_user = 0
-
-    try:
-        while True:
-            # Бесконечно обрабатываем входящие подключения
-            client_sock, client_addr = serv_sock.accept()
-
-            print('client connected', client_addr)
-            # карточки ненадолго переворачиваются
-            while True:
-                match current_user:
-                    case 0:
-                        # Получаем сообщение от клиента
-                        data = client_sock.recv(1024)
-                        if not data:  # Клиент отключился
-                            break
-                        message1 = data.decode('utf-8')
-                        print(message1)
-
-                        data = client_sock.recv(1024)
-                        if not data:  # Клиент отключился
-                            break
-                        message2 = data.decode('utf-8')
-                        print(message2)
-                        # if message1.image != message2.image:
-                        #   current_user = 1
-
-                    case 1:
-                        # Отправляем сообщение клиенту
-                        response1 = input()
-                        client_sock.sendall(response1.encode('utf-8'))
-                        response2 = input()
-                        client_sock.sendall(response2.encode('utf-8'))
-                        # заблокировать все кнопки для нажатия, если неправильно
-                        # картинки открыть если правильно и продолжать отсылать
-                        # if response1.image != response2.image:
-                        #   current_user = 0
-
-            client_sock.close()
-
-    except (KeyboardInterrupt, SystemExit):
-        print('Прерывание программы.')
-
-    except Exception as e:
-        print(f'Произошла ошибка: {e}')
-
-
 
 class MainWindow(QMainWindow):
     def __init__(self, current_user):
         super().__init__()
-        # Загрузить пользовательский интерфейс главного окна из файла .ui
         uic.loadUi('MainWindow.ui', self)
         self.setWindowTitle("Мемо - игра для вас и ваших друзей")
         self.setWindowIcon(QIcon('pictures/icon.png'))
 
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создаем сокет
-        self.sock.bind(('', 55000))  # связываем сокет с портом, где он будет ожидать сообщения
-        self.sock.listen(1)  # указываем сколько может сокет принимать соединений
-        print('Server is running')
+        self.buttons_list = self.buttons.buttons()
+        self.current_user = current_user  # 0 - клиент, 1 - сервер
+        self.press_count = 0  # Количество нажатий
 
+        # Присваиваем кнопкам случайные картинки
         pictures = [f'pictures/{i + 1}.jpg' for i in range(15)]
+        self.buttons = self.buttons.buttons()
         random.shuffle(pictures)
-
-        new_buttons = self.buttons.buttons()
-        random.shuffle(new_buttons)
-        self.slots = {}
+        random.shuffle(self.buttons)
         for i in range(15):
             for k in range(2):
-                button = new_buttons[i * 2 + k]
+                button = self.buttons[i * 2 + k]
                 button.setIcon(QIcon('pictures/back.jpg'))
                 button.setText('')
-                button.setIconSize(QSize(button.width() - 6, button.height() - 6))
+                print(button.objectName(), pictures[i])
+                button.setIconSize(QSize(button.width() - 8, button.height() - 8))
                 button.setProperty('picture', pictures[i])
-                button.clicked.connect(self.button_clicked)
+                # button.clicked.connect(self.button_clicked)
+        self.enemys_turn.setStyleSheet("""QPushButton{
+                                    border: 5px solid #721;
+                                    border-style: outset;
+                                    background: rgb(180,50,40);
+                                    color: rgb(250,250,255);
+                                    }""")
 
-        #for button in self.buttons.buttons():
-        #    print(f"Button {button.objectName()} has picture {button.property('picture')}")
+        # Подключение к серверу
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_sock.bind(('', 53210))  # Укажите IP сервера, если нужно
+        self.server_sock.listen(1)
+        print('Server is running')
 
-    def button_clicked(self, current_user):
-        button = self.sender()
-        button.setIcon(QIcon(f"{button.property('picture')}"))
+        self.client_sock, self.client_addr = self.server_sock.accept()
+        print('Client connected', self.client_addr)
 
+        self.game_active = True  # Игра активна для клиента
+        threading.Thread(target=self.handle_client, daemon=True).start()
+
+    def handle_client(self):
+        try:
+            while True:
+                # Получаем информацию о нажатии кнопки от клиента
+                data = self.client_sock.recv(1024)
+                if not data:
+                    break
+                button_name = data.decode('utf-8')
+                button = self.findChild(QPushButton, button_name)
+                # Генерируем путь к изображению
+                picture_path = button.property('picture')
+
+                # Отправляем информацию клиенту для синхронного открытия
+                self.client_sock.sendall(picture_path.encode('utf-8'))
+
+                # Сервер также обновляет свою версию интерфейса
+                print(f"Button {button_name} pressed, displaying picture {picture_path}")
+                self.update_server_interface(button_name, picture_path)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            self.client_sock.close()  # Закрываем соединение
+
+    def update_server_interface(self, button_name, picture_path):
+        button = self.findChild(QPushButton, button_name)
+        button.setIcon(QIcon(picture_path))
 
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
-    window = MainWindow(0)
+    window = MainWindow(0)  # 0 означает, что клиент ходит первым
     window.show()
     sys.exit(app.exec())
+
+
